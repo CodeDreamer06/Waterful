a6e9e8f00d764a9afbbfdd2dd74a8e1d4d3dca21
Abhinav <codedreamer06@gmail.com>
2022-07-25 20:18:51 +0530

    Added counter

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,4 +4,20 @@ window.addEventListener('load', () => {
     spinner.parentElement.removeChild(spinner);
     spinnerBackground.parentElement.removeChild(spinnerBackground);
 });
+
 $(() => $('[data-toggle="tooltip"]').tooltip());
+
+$('.counter').each((_, self) => {
+    var text = $(self).text();
+    text = text.replace(text.includes("ml") ? "ml" : "L", "");
+    text = parseFloat(text);
+    console.log(text);
+
+    jQuery({
+        Counter: 0
+    }).animate({ Counter: text }, {
+        duration: 2000,
+        easing: 'swing',
+        step: () => $(self).text(Math.ceil(this.Counter))
+    });
+});
