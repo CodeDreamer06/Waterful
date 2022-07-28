f1d122be74cf058382101735b273e8e1e800125f
Abhinav <codedreamer06@gmail.com>
2022-07-28 07:04:05 +0530

    Removed counter

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,21 +3,5 @@ let spinnerBackground = document.querySelector('.spinner-background');
 window.addEventListener('load', () => {
     spinner.parentElement.removeChild(spinner);
     spinnerBackground.parentElement.removeChild(spinnerBackground);
-});
-
-$(() => $('[data-toggle="tooltip"]').tooltip());
-
-$('.counter').each((_, self) => {
-    var text = $(self).text();
-    text = text.replace(text.includes("ml") ? "ml" : "L", "");
-    text = parseFloat(text);
-    console.log(text);
-
-    jQuery({
-        Counter: 0
-    }).animate({ Counter: text }, {
-        duration: 2000,
-        easing: 'swing',
-        step: () => $(self).text(Math.ceil(this.Counter))
-    });
+    $(() => $('[data-toggle="tooltip"]').tooltip());
 });
