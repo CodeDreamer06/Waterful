746402775bf525af3a10689df0f97bba060b877e
Abhinav <codedreamer06@gmail.com>
2022-07-29 22:52:41 +0530

    Updated badge counter JS

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,33 @@
 ﻿let spinner = document.querySelector('.spinner');
 let spinnerBackground = document.querySelector('.spinner-background');
+
 window.addEventListener('load', () => {
     spinner.parentElement.removeChild(spinner);
     spinnerBackground.parentElement.removeChild(spinnerBackground);
     $(() => $('[data-toggle="tooltip"]').tooltip());
+});
+
+let miniGlassQuantity = 0, glassQuantity = 0, bottleQuantity = 0;
+
+$(document).ready(() => {
+    document.getElementById("drinkOptionCounter--mini-glass").style.display = "none";
+    document.getElementById("drinkOptionCounter--glass").style.display = "none";
+    document.getElementById("drinkOptionCounter--bottle").style.display = "none";
+    $("#drinkOption--mini-glass").click(() => {
+        miniGlassQuantity++;
+        document.getElementById("drinkOptionCounter--mini-glass").style.display = "inline-block";
+        $("#drinkOptionCounter--mini-glass").text(miniGlassQuantity);
+    });
+
+    $("#drinkOption--glass").click(() => {
+        glassQuantity++;
+        document.getElementById("drinkOptionCounter--glass").style.display = "inline-block";
+        $("#drinkOptionCounter--glass").text(glassQuantity);
+    });
+
+    $("#drinkOption--bottle").click(() => {
+        bottleQuantity++;
+        document.getElementById("drinkOptionCounter--bottle").style.display = "inline-block";
+        $("#drinkOptionCounter--bottle").text(bottleQuantity);
+    });
 });
